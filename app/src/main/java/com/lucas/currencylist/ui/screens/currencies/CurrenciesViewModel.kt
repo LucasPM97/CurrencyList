0847eaf647aaf0715fcebbc858264a6ff2975722
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2021-09-11 20:55:01 -0300

    Fix ViewModel no commited

--- app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesViewModel.kt
+++ app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesViewModel.kt
@@ -1,7 +1,16 @@
 package com.lucas.currencylist.ui.screens.currencies
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.lucas.currencylist.models.CurrencyValue
+import com.lucas.currencylist.models.repositories.CurrencyRepository
+import com.lucas.currencylist.models.repositories.ICurrencyRepository
 
 class CurrenciesViewModel : ViewModel() {
 
+    private val repository: ICurrencyRepository = CurrencyRepository()
+
+    private val _currencies = MutableLiveData<List<CurrencyValue>>()
+    val currencies = repository.getCurrencyValues().asLiveData()
 }
