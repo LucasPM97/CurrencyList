e9d3a09ae10ff693580727edbf09591e807bf296
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-10-24 20:59:10 -0300

    Add Fav button to currencies

---
+++ app/src/main/java/com/lucas/currencylist/ui/components/FavButton.kt
@@ -0,0 +1,25 @@
+package com.lucas.currencylist.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FavButton(fav: Boolean, onClick: () -> Unit) {
+    IconButton(
+        onClick,
+        Modifier.padding(start = 5.dp)
+    ) {
+        Icon(
+            imageVector = if (fav) Icons.Filled.Clear
+            else Icons.Filled.Add,
+            contentDescription = "Add to favorites"
+        )
+    }
+}
