cd1a576c27f9d81b376871f91dbcec5adb572d81
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-11-13 22:30:48 -0300

    Show Faved Currencies

---
+++ app/src/main/java/com/lucas/currencylist/ui/screens/favCurrencies/FavCurrenciesViewModel.kt
@@ -0,0 +1,27 @@
+package com.lucas.currencylist.ui.screens.favCurrencies
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import com.lucas.core.database.CurrenciesDatabase
+import com.lucas.core.repositories.CurrencyRepository
+import com.lucas.core.repositories.ICurrencyRepository
+import com.lucas.core.services.RetrofitBuilder
+
+class FavCurrenciesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: ICurrencyRepository = CurrencyRepository(
+        CurrenciesDatabase.getInstance(application.applicationContext).dao,
+        RetrofitBuilder.buenbitService,
+        RetrofitBuilder.binanceService,
+        RetrofitBuilder.ripioService
+    )
+
+    val tradingWebProviders = repository.getTradingWebProviders()
+
+    val currencies = repository.getFavCurrencies()
+
+    suspend fun updateFav(currencyId: String, fav: Boolean) {
+        repository.updateCurrencyFav(currencyId, fav)
+    }
+}
