ebe57a30d426f4fd57994e20c00a63a9c245e480
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-11-13 21:56:15 -0300

    Simplify CurrencyList management and Create TradingWebList component

--- app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesScreen.kt
+++ app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesScreen.kt
@@ -1,13 +1,10 @@
 package com.lucas.currencylist.ui.screens.currencies
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,9 +12,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.lucas.core.models.CurrencyValue
-import com.lucas.core.models.TradingPlatformType
-import com.lucas.core.models.TradingWebProvider
-import com.lucas.currencylist.ui.components.TradingWebCard
+import com.lucas.currencylist.ui.components.TradingWebList
 import kotlinx.coroutines.launch
 
 val itemsSpace = 20.dp
@@ -30,59 +25,32 @@ fun CurrenciesScreen(
 
     val coroutineScope = rememberCoroutineScope()
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(
-                horizontal = 20.dp,
-            )
-            .padding(bottom = 20.dp)
-            .verticalScroll(rememberScrollState())
-
-    ) {
-        viewModel.currencies.forEach { (platform, currenciesState) ->
-            val currencies by currenciesState.observeAsState(initial = emptyList())
-
-            val providerState = viewModel.tradingWebProviders.first {
-                it.platformType == platform
-            }
-
-            RenderTradingWeb(
-                tradingWebProvider = providerState,
-                platformType = platform,
-                currencies = currencies,
-                itemFavOnClick = { currencyId ->
-                    coroutineScope.launch {
-                        val currency = currencies.first {
-                            it.currencyId == currencyId
-                        }
-                        viewModel.updateFav(currencyId, !currency.fav)
-                    }
-                }
-            )
+    fun favCurrency(currency: CurrencyValue) {
+        coroutineScope.launch {
+            viewModel.updateFav(currency.currencyId, !currency.fav)
         }
     }
-}
 
-@Composable
-fun RenderTradingWeb(
-    tradingWebProvider: TradingWebProvider,
-    platformType: TradingPlatformType,
-    currencies: List<CurrencyValue>,
-    modifier: Modifier = Modifier,
-    itemFavOnClick: (currencyId: String) -> Unit
-) {
-    val tradingWebState by tradingWebProvider.state.observeAsState()
+    val platformState = viewModel.currencies.map { (platform, currenciesFlow) ->
+        val currencies by currenciesFlow.collectAsState(initial = emptyList())
 
-    tradingWebState?.let {
-        TradingWebCard(
-            platformType,
-            tradingWebState = it,
-            currencyList = currencies,
-            itemFavOnClick = itemFavOnClick,
-            modifier.padding(top = itemsSpace)
-        )
+        val providerState = viewModel.tradingWebProviders.first {
+            it.platformType == platform
+        }
+
+        providerState to currencies
     }
+
+    TradingWebList(
+        platformState,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp)
+            .padding(bottom = 20.dp),
+        itemFavOnClick = {
+            favCurrency(it)
+        }
+    )
 }
 
 @Preview(showBackground = true)
