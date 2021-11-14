8039adb4083dff41b43e06d34cb8cc6a84f53521
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-11-14 00:32:00 -0300

    Only update ExchangeValue when currency is already stored

--- core/src/main/java/com/lucas/core/database/CurrenciesDatabaseDAO.kt
+++ core/src/main/java/com/lucas/core/database/CurrenciesDatabaseDAO.kt
@@ -14,11 +14,14 @@ interface CurrenciesDatabaseDAO {
     @Query("Select * FROM currencies_table WHERE platform == :platformType AND fav == 1")
     fun getFavCurrenciesFlowFromPlatform(platformType: TradingPlatformType): Flow<List<CurrencyValue>>
 
-    @Query("Select * FROM currencies_table WHERE platform == :platformType")
-    suspend fun getAllCurrenciesFromPlatform(platformType: TradingPlatformType): List<CurrencyValue>
+    @Query("Select * FROM currencies_table WHERE currencyId == :currencyId")
+    fun getCurrencyById(currencyId: String): CurrencyValue?
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertOrUpdateList(currencies: List<CurrencyValue>)
+    @Insert
+    suspend fun insertCurrency(currency: CurrencyValue)
+
+    @Query("UPDATE currencies_table SET exchangeValue=:exchangeValue WHERE currencyId = :currencyId")
+    suspend fun updateExchangeValues(currencyId: String, exchangeValue: Double)
 
     @Query("UPDATE currencies_table SET fav=:fav WHERE currencyId = :currencyId")
     suspend fun updateFav(currencyId: String, fav: Boolean)
