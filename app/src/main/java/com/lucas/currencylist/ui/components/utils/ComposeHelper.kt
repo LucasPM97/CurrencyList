f279352755757d4ea8dcb1049092b83cf26ad9f8
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-11-27 19:52:44 -0300

    Simplify ViewModel implementation on Composables

---
+++ app/src/main/java/com/lucas/currencylist/ui/components/utils/ComposeHelper.kt
@@ -0,0 +1,24 @@
+package com.lucas.currencylist.ui.components.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.lucas.core.models.CurrencyValue
+import com.lucas.core.models.TradingPlatformType
+import com.lucas.core.models.TradingWebProvider
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun mapToState(
+    currenciesMap: Map<TradingPlatformType, Flow<List<CurrencyValue>>>,
+    tradingWebProviders: List<TradingWebProvider>
+) =
+    currenciesMap.map { (platform, currenciesFlow) ->
+        val currencies by currenciesFlow.collectAsState(initial = emptyList())
+
+        val providerState = tradingWebProviders.first {
+            it.platformType == platform
+        }
+
+        providerState to currencies
+    }
