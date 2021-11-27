c6ecb6c6c37cf5277ce50eec7def32ae20f4efb8
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2021-11-27 20:39:59 -0300

    Add AppBar to Currencies screen with back button

--- app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesScreen.kt
+++ app/src/main/java/com/lucas/currencylist/ui/screens/currencies/CurrenciesScreen.kt
@@ -2,9 +2,8 @@ package com.lucas.currencylist.ui.screens.currencies
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,10 +11,8 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.lucas.core.models.CurrencyValue
-import com.lucas.core.models.TradingPlatformType
 import com.lucas.currencylist.ui.components.TradingWebList
 import com.lucas.currencylist.ui.components.utils.mapToState
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 @Composable
@@ -23,6 +20,22 @@ fun CurrenciesScreen(
     navController: NavController?,
     viewModel: CurrenciesViewModel = viewModel()
 ) {
+    fun onBack() {
+        navController?.let {
+            it.popBackStack()
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopBar(onBack = { onBack() })
+        },
+        content = { Screen(viewModel) }
+    )
+}
+
+@Composable
+private fun Screen(viewModel: CurrenciesViewModel) {
     val coroutineScope = rememberCoroutineScope()
 
     fun favCurrency(currency: CurrencyValue) {
@@ -41,8 +54,7 @@ fun CurrenciesScreen(
         platformState,
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 20.dp)
-            .padding(bottom = 20.dp),
+            .padding(horizontal = 20.dp),
         itemFavOnClick = {
             favCurrency(it)
         }
---
+++ app/src/main/java/com/lucas/currencylist/ui/screens/currencies/TopBar.kt
@@ -0,0 +1,32 @@
+package com.lucas.currencylist.ui.screens.currencies
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TopBar(onBack: () -> Unit) {
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back",
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun PreviewTopBar() {
+    TopBar(
+        onBack = {}
+    )
+}
