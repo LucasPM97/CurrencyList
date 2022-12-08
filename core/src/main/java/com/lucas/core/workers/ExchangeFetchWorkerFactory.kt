9bebb81eddbb03cf4d9dd4b5cc6907300acf16c9
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2022-12-08 14:19:53 -0300

    Add Repository as dependency on FetchWorker

---
+++ core/src/main/java/com/lucas/core/workers/ExchangeFetchWorkerFactory.kt
@@ -0,0 +1,24 @@
+package com.lucas.core.workers
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import com.lucas.core.data.repositories.ICurrencyRepository
+
+class ExchangeFetchWorkerFactory(private val repository: ICurrencyRepository) : WorkerFactory() {
+
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+
+        return ExchangeFetchWorker(
+            appContext,
+            workerParameters,
+            repository
+        )
+
+    }
+}
