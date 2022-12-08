40fd507bb05bcc95c2bc32e23d07e703d43c2b49
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2022-12-08 14:19:53 -0300

    Add Repository as dependency on FetchWorker

--- app/src/main/java/com/lucas/currencylist/ui/screens/favCurrencies/FavCurrenciesViewModel.kt
+++ app/src/main/java/com/lucas/currencylist/ui/screens/favCurrencies/FavCurrenciesViewModel.kt
@@ -2,10 +2,7 @@ package com.lucas.currencylist.ui.screens.favCurrencies
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.lucas.core.data.local.ExchangeLocalDataSource
 import com.lucas.core.data.local.database.CurrenciesDatabase
 import com.lucas.core.data.local.database.PlatformUpdatesDatabase
@@ -14,6 +11,7 @@ import com.lucas.core.data.repositories.CurrencyRepository
 import com.lucas.core.data.repositories.ICurrencyRepository
 import com.lucas.core.data.remote.apis.RetrofitBuilder
 import com.lucas.core.workers.ExchangeFetchWorker
+import java.util.concurrent.TimeUnit
 
 class FavCurrenciesViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -37,16 +35,19 @@ class FavCurrenciesViewModel(application: Application) : AndroidViewModel(applic
     private val workManager = WorkManager.getInstance(application)
 
     init {
-        val request = OneTimeWorkRequestBuilder<ExchangeFetchWorker>()
-            .setConstraints(
-                Constraints.Builder()
-                    .setRequiredNetworkType(NetworkType.CONNECTED)
-                    .setRequiresBatteryNotLow(true)
-                    .build()
-            )
-            .addTag(ExchangeFetchWorker.NAME)
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
             .build()
-        workManager.beginWith(request).enqueue()
+        val request = PeriodicWorkRequestBuilder<ExchangeFetchWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .addTag(ExchangeFetchWorker.TAG)
+            .build()
+        workManager.enqueueUniquePeriodicWork(
+            ExchangeFetchWorker.TAG,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            request
+        )
     }
 
     suspend fun updateFav(currencyId: String, fav: Boolean) {
