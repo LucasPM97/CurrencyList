c77896efc8c92aeceea5eebbf440b808ab337fb6
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-12-14 17:15:43 -0300

    Fix DataBase module & Remove Database classes unused code

--- core/src/main/java/com/lucas/core/data/local/database/exchangeValues/ExchangeValueDatabase.kt
+++ core/src/main/java/com/lucas/core/data/local/database/exchangeValues/ExchangeValueDatabase.kt
@@ -1,41 +1,14 @@
 package com.lucas.core.data.local.database.exchangeValues
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.lucas.core.data.models.ExchangeValue
 import com.lucas.core.data.local.database.converters.DateTimeConverter
-
+import com.lucas.core.data.models.ExchangeValue
 
 @Database(entities = [ExchangeValue::class], version = 1, exportSchema = false)
 @TypeConverters(DateTimeConverter::class)
 abstract class ExchangeValueDatabase : RoomDatabase() {
 
     abstract val dao: ExchangeValuesDatabaseDAO
-
-    companion object {
-
-        @Volatile
-        private var INSTANCE: ExchangeValueDatabase? = null
-
-        fun getInstance(context: Context): ExchangeValueDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        ExchangeValueDatabase::class.java,
-                        "currencies_database"
-                    )
-                        .fallbackToDestructiveMigration()
-                        .build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
-        }
-    }
 }
