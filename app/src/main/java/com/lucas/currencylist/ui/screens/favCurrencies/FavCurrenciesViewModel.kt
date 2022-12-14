acc923d2a7dacc9448e6bc6ef4a50393bc2ee16e
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-12-14 16:36:22 -0300

    Add FavCurrenciesViewModel to App module

--- app/src/main/java/com/lucas/currencylist/ui/screens/favCurrencies/FavCurrenciesViewModel.kt
+++ app/src/main/java/com/lucas/currencylist/ui/screens/favCurrencies/FavCurrenciesViewModel.kt
@@ -1,55 +1,36 @@
 package com.lucas.currencylist.ui.screens.favCurrencies
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.work.*
-import com.lucas.core.data.local.ExchangeLocalDataSource
-import com.lucas.core.data.local.database.exchangeValues.ExchangeValueDatabase
-import com.lucas.core.data.local.database.platforms.PlatformUpdatesDatabase
-import com.lucas.core.data.remote.ExchangeRemoteDataSource
-import com.lucas.core.data.remote.apis.RetrofitBuilder
-import com.lucas.core.data.repositories.CurrencyRepository
 import com.lucas.core.data.workers.ExchangeFetchWorker
-import com.lucas.core.domain.useCases.GetFavExchangeValuesUseCase
-import com.lucas.core.domain.useCases.UpdateExchangeValueFavUseCase
+import com.lucas.core.domain.useCases.IGetFavExchangeValuesUseCase
+import com.lucas.core.domain.useCases.IUpdateExchangeValueFavUseCase
 import java.util.concurrent.TimeUnit
 
-class FavCurrenciesViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val repository = CurrencyRepository(
-        ExchangeLocalDataSource(
-            PlatformUpdatesDatabase.getInstance(application.applicationContext).dao,
-            ExchangeValueDatabase.getInstance(application.applicationContext).dao
-        ),
-        ExchangeRemoteDataSource(
-            RetrofitBuilder.buenbitService,
-            RetrofitBuilder.binanceApi,
-            RetrofitBuilder.ripioService
-        )
-
-    )
-    private val getFavExchangeValues = GetFavExchangeValuesUseCase(repository)
-    private val updateExchangeValueFav = UpdateExchangeValueFavUseCase(repository)
+class FavCurrenciesViewModel(
+    private val getFavExchangeValues: IGetFavExchangeValuesUseCase,
+    private val updateExchangeValueFav: IUpdateExchangeValueFavUseCase
+) : ViewModel() {
 
     val currencies = getFavExchangeValues()
 
-    private val workManager = WorkManager.getInstance(application)
-
-    init {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .setRequiresBatteryNotLow(true)
-            .build()
-        val request = PeriodicWorkRequestBuilder<ExchangeFetchWorker>(15, TimeUnit.MINUTES)
-            .setConstraints(constraints)
-            .addTag(ExchangeFetchWorker.TAG)
-            .build()
-        workManager.enqueueUniquePeriodicWork(
-            ExchangeFetchWorker.TAG,
-            ExistingPeriodicWorkPolicy.REPLACE,
-            request
-        )
-    }
+//    private val workManager = WorkManager.getInstance(application)
+//
+//    init {
+//        val constraints = Constraints.Builder()
+//            .setRequiredNetworkType(NetworkType.CONNECTED)
+//            .setRequiresBatteryNotLow(true)
+//            .build()
+//        val request = PeriodicWorkRequestBuilder<ExchangeFetchWorker>(15, TimeUnit.MINUTES)
+//            .setConstraints(constraints)
+//            .addTag(ExchangeFetchWorker.TAG)
+//            .build()
+//        workManager.enqueueUniquePeriodicWork(
+//            ExchangeFetchWorker.TAG,
+//            ExistingPeriodicWorkPolicy.REPLACE,
+//            request
+//        )
+//    }
 
     suspend fun updateFav(currencyId: String, fav: Boolean) {
         updateExchangeValueFav(currencyId, fav)
