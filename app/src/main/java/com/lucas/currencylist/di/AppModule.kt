011b4d9ef309f44a173d8a7723063a9f1f3f8a18
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-12-14 16:55:37 -0300

    Add all ViewModels to App module

--- app/src/main/java/com/lucas/currencylist/di/AppModule.kt
+++ app/src/main/java/com/lucas/currencylist/di/AppModule.kt
@@ -1,12 +1,19 @@
 package com.lucas.currencylist.di
 
-import com.lucas.currencylist.ui.screens.favCurrencies.FavCurrenciesViewModel
+import com.lucas.currencylist.ui.screens.exchangeList.ExchangeListViewModel
+import com.lucas.currencylist.ui.screens.favExchangeList.FavExchangeListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel {
-        FavCurrenciesViewModel(
+        FavExchangeListViewModel(
+            get(),
+            get()
+        )
+    }
+    viewModel {
+        ExchangeListViewModel(
             get(),
             get()
         )
