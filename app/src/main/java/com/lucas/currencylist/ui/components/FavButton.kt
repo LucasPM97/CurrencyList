bb81c68159e834afcff0289ebd6168bea57fefcb
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-12-21 11:51:02 -0300

    Update libraries and add some gradle settings to be able to run UI tests

--- app/src/main/java/com/lucas/currencylist/ui/components/FavButton.kt
+++ app/src/main/java/com/lucas/currencylist/ui/components/FavButton.kt
@@ -1,11 +1,11 @@
 package com.lucas.currencylist.ui.components
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
